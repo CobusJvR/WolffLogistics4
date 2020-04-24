0b15f1c13d06b6b8fc708002f7c3492f0b40155f
cobusjvr <cobusjvr@gmail.com>
2020-04-24 04:33:25 +0200

    Add project files.

---
+++ WolffLogistics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WolffLogistics.MvcApplication" Language="C#" %>
---
+++ WolffLogistics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WolffLogistics.Startup))]
+namespace WolffLogistics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
