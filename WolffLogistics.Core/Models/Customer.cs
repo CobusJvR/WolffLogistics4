34cb0f38791ac88c3f01bfafa4d00222e0798dea
cobusjvr <cobusjvr@gmail.com>
2020-04-24 09:57:53 +0200

    Added SQL repository, Unity configuration, Customer CRUD and views

--- WolffLogistics.Core/Models/Customer.cs
+++ WolffLogistics.Core/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace WolffLogistics.Core.Models
 {
     public class Customer : BaseEntity
     {
+        [DisplayName("Acc No")]
         public string AccountNumber { get; set; }
+        [DisplayName("Company")]
         public string CompanyName { get; set; }
+        [DisplayName("Registration No")]
         public string RegistrationNumber { get; set; }
+        [DisplayName("VAT No")]
         public string VatNumber { get; set; }
+        [DisplayName("Tel No")]
         public string TelephoneNumber { get; set; }
         public string Email { get; set; }
         public string Street { get; set; }
---
+++ WolffLogistics.WebUI/Controllers/CustomerManagerController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WolffLogistics.Core.Contracts;
+using WolffLogistics.Core.Models;
+using WolffLogistics.DataAccess.SQL;
+
+namespace WolffLogistics.WebUI.Controllers
+{
+    public class CustomerManagerController : Controller
+    {
+        IRepository<Customer> context;
+        // GET: CustomerManager
+        public CustomerManagerController(IRepository<Customer> customerContext)
+        {
+            context = customerContext;
+        }
+        public ActionResult Index()
+        {
+            List<Customer> customers = context.Collection().ToList();
+            return View(customers);
+        }
+        public ActionResult Create()
+        {
+            Customer customer = new Customer();
+            return View(customer);
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(customer);
+            }
+            else
+            {
+                context.Insert(customer);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Edit(string Id)
+        {
+            Customer customer = context.Find(Id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(Customer customer, string Id)
+        {
+            Customer customerToEdit = context.Find(Id);
+            if (customerToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(customer);
+                }
+                customerToEdit.AccountNumber = customer.AccountNumber;
+                customerToEdit.City = customer.City;
+                customerToEdit.CompanyName = customer.CompanyName;
+                customerToEdit.Email = customer.Email;
+                customerToEdit.PostalCode = customer.PostalCode;
+                customerToEdit.Province = customer.Province;
+                customerToEdit.RegistrationNumber = customer.RegistrationNumber;
+                customerToEdit.Street = customer.Street;
+                customerToEdit.TelephoneNumber = customer.TelephoneNumber;
+                customerToEdit.VatNumber = customer.VatNumber;
+
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Delete(string Id)
+        {
+            Customer customerToDelete = context.Find(Id);
+            if (customerToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customerToDelete);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Customer customerToDelete = context.Find(Id);
+            if (customerToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
