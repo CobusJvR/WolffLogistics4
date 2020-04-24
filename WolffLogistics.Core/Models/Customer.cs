ade79ca4946e5439cc55b5cb77b88e62b5e2e81f
cobusjvr <cobusjvr@gmail.com>
2020-04-24 06:34:57 +0200

    Added Customer class

---
+++ WolffLogistics.Core/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolffLogistics.Core.Models
+{
+    public class Customer : BaseEntity
+    {
+        public string AccountNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string VatNumber { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ WolffLogistics.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WolffLogistics.Core.Models;
+
+namespace WolffLogistics.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base ("DefaultConnection")
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
